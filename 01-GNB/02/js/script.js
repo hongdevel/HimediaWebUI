33bb522a26ae0df085b88c63c77ab930e466af51
hongdevel <hongjinyoung15@gmail.com>
2024-10-04 10:47:04 +0900

    최종 완성

--- 01-GNB/02/js/script.js
+++ 01-GNB/02/js/script.js
@@ -3,6 +3,7 @@ $(function () {
     const $menu = $(".gnb > li");
     const $submenu = $(".submenu");
     const duration = 300;
+    const $window = $(window);
 
     // GNB
     $menu.on("mouseenter", function () {
@@ -14,5 +15,16 @@ $(function () {
         $(this).removeClass("on");
         $header.removeClass("active");
         $submenu.stop().slideUp(duration);
+    });
+    // 마우스 휠을 조작했을 때
+    $window.on("wheel", function (e) {
+        console.log(e);
+        if (e.originalEvent.wheelData > 0) {
+            // 휠 올림
+            $header.removeClass("hide");
+        } else {
+            // 휠 내림
+            $header.addClass("hide");
+        }
     });
 });
